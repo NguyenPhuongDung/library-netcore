a7d97e88c1cfc1c1f828f40faa6822e0385a154d
Nguyen Dung <dungitbk@outlook.com>
2019-07-24 17:30:28 +0700

    add readme

--- Library.Api/Controllers/ValuesController.cs
+++ Library.Api/Controllers/ValuesController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Library.Entities;
 namespace Library.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        ValuesController(){
-            var context = new LibraryContext();
+        private readonly LibraryContext _context;
+        public ValuesController(LibraryContext context){
+            _context = context;
         }
        
         // GET api/values
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# DI
+# Git
+# Visual Studio Code cli
+## Init solution 
+//new solution
+dotnet new sln -o Library
+//new project
+dotnet new classlib -o Library.Entities
+//add project into solution
+dotnet sln Library.sln add .\Library.Entities\Library.Entities.csproj
+//add reference 
+dotnet add reference ../Library.Entities/Library.Entities.csproj
 example about dependency injection by C#
